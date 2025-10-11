5bea4bd42ebdc607701e200d8c98e5803c43d5c7
mikey mike <mikaelworld25@gmail.com>
2025-10-11 14:19:17 +0200

    outputs

--- Second/bit_generator.py
+++ Second/bit_generator.py
@@ -8,8 +8,3 @@ class BitGenerator:
     @staticmethod
     def generate(length):
         return ''.join(random.choice('01') for _ in range(length))
-    
-    @staticmethod
-    def generate_from_text(text):
-        """Конвертация текста в биты (если понадобится)"""
-        return ''.join(format(ord(char), '08b') for char in text)
